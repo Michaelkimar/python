7a4cbf245284eaa674d5419cf232fc1170b4fec8
michaelkimaru <mgkimaru@gmail.com>
2023-02-10 10:52:49 +0300

    python lists

---
+++ lists/list1.py
@@ -0,0 +1,45 @@
+# #my first python list
+# names=["mark","ian","joel"]
+# numbers=[2,4,6,4,8,4,5,6]
+# decimals=[6.6,7.9,4.6,5.9,5.3]
+# range=(len(numbers))
+# for i in range(len(numbers)):
+#     print(numbers[i])
+# names.append("kim")
+# print(names)
+# for i in range(len(numbers)):
+#     print(numbers[i])
+#Index
+#append
+#sort
+#count
+#pop-removes an element at a given index and returns that element    
+#insert
+#remove
+# print(numbers)
+# print(numbers.count(4))
+# print(numbers.pop(2))
+# numbers.insert(2,100)
+# for i in range(len(numbers)):
+
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+# zero=[0]*50
+# print(zero)
+# list1=(89,90)
+# list2=(91,92)
+# finallist=list1+list2
+# print(finallist)
+# nums=(23,45)
+# # max(nums)
+# for i in range(10):
+#     nums.append(i)
+# for i in range (len(nums)):
+#     print(nums[i])                  
+
+nums=[]
+for num in range (20):
+    if (num%2)==0:
+        nums.append(num)
+print(nums)        
